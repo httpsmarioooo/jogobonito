dee22bfd67b80da4003c3408f84ac08d75a29e68
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-09-30 08:58:06 -0500

    Bobadas

--- src/main/java/com/league/jogobonito/controller/JudgesPerMatchController.java
+++ src/main/java/com/league/jogobonito/controller/JudgesPerMatchController.java
@@ -1,7 +1,5 @@
 package com.league.jogobonito.controller;
-import com.league.jogobonito.domain.JudgeRole;
 import com.league.jogobonito.domain.JudgesPerMatch;
-import com.league.jogobonito.repository.JudgeRoleRepository;
 import com.league.jogobonito.repository.JudgesPerMatchRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
