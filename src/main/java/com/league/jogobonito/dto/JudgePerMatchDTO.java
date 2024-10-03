b87b53c437835293ff69ce9ce5c91c7aac60e63f
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-10-02 20:47:54 -0500

    JudgePerMatchDTO creacion

--- src/main/java/com/league/jogobonito/dto/JudgePerMatchDTO.java
+++ src/main/java/com/league/jogobonito/dto/JudgePerMatchDTO.java
@@ -1,4 +1,21 @@
 package com.league.jogobonito.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class JudgePerMatchDTO {
+
+    private Integer id;
+    private Integer matchId;
+    private Integer judgeId;
+    private Integer roleId;
+
+
 }
