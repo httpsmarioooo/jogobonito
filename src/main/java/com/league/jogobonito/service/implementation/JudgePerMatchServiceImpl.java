548638db1345c08018a757897361e19d0c8dd84d
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-10-08 01:42:01 -0500

    //Ultimas validaciones en el mismo impl en la parte final---

--- src/main/java/com/league/jogobonito/service/implementation/JudgePerMatchServiceImpl.java
+++ src/main/java/com/league/jogobonito/service/implementation/JudgePerMatchServiceImpl.java
@@ -1,12 +1,34 @@
 package com.league.jogobonito.service.implementation;
 
-
+import com.league.jogobonito.domain.JudgePerMatch;
+import com.league.jogobonito.domain.Judge;
+import com.league.jogobonito.domain.JudgeRole;
+import com.league.jogobonito.domain.Match;
 import com.league.jogobonito.dto.JudgePerMatchDTO;
+import com.league.jogobonito.mapper.JudgePerMatchMapper;
+import com.league.jogobonito.repository.*;
 import com.league.jogobonito.service.JudgePerMatchService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JudgePerMatchServiceImpl implements JudgePerMatchService {
+
+    private final JudgePerMatchRepository judgePerMatchRepository;
+
+    private final MatchRepository matchRepository;
+    private final JudgeRepository judgeRepository;
+    private final JudgeRoleRepository judgeRoleRepository;
+
+
+
+    public JudgePerMatchServiceImpl(JudgePerMatchRepository judgePerMatchRepository, MatchRepository matchRepository, JudgeRepository judgeRepository,JudgeRoleRepository judgeRoleRepository) {
+        this.judgePerMatchRepository = judgePerMatchRepository;
+        this.matchRepository = matchRepository;
+        this.judgeRepository = judgeRepository;
+        this.judgeRoleRepository = judgeRoleRepository;
+
+    }
+
     @Override
     public JudgePerMatchDTO guardarNuevoJudgePerMatch(JudgePerMatchDTO judgePerMatchDTO) throws Exception {
 
@@ -15,7 +37,6 @@ public class JudgePerMatchServiceImpl implements JudgePerMatchService {
         if(judgePerMatchDTO.getId() != null) {
             throw new Exception("El id debe de ser nulo");
         }
-
 
         //2. Validaciones dependencias, llaves etc
 
@@ -33,9 +54,32 @@ public class JudgePerMatchServiceImpl implements JudgePerMatchService {
 
 
 
+        //Ultimas validaciones en el mismo impl en la parte final---
+        JudgePerMatch judgePerMatch = JudgePerMatchMapper.dtoToDomain(judgePerMatchDTO);
+        Match match = matchRepository.getReferenceById(judgePerMatchDTO.getMatchId());
+        Judge judge = judgeRepository.getReferenceById(judgePerMatchDTO.getJudgeId());
+        JudgeRole judgeRole = judgeRoleRepository.getReferenceById(judgePerMatchDTO.getRoleId());
+
+        if (match == null){
+            throw new Exception("El Match no existe");
+        }
 
 
+        if (judge == null){
+            throw new Exception("El Judge no existe");
+        }
 
-        return null;
+
+        if (judgeRole == null){
+            throw new Exception("El JudgeRole no existe");
+        }
+
+        judgePerMatch.setMatch(match);
+        judgePerMatch.setJudge(judge);
+        judgePerMatch.setJudgeRole(judgeRole);
+
+        judgePerMatch = judgePerMatchRepository.save(judgePerMatch);
+
+        return JudgePerMatchMapper.domainToDto(judgePerMatch);
     }
 }
