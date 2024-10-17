194410c0b2ec6d5c2fb3d333ac29d35e29d9a41a
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2024-10-16 20:25:26 -0500

    xd

--- src/main/java/com/league/jogobonito/service/PlayerService.java
+++ src/main/java/com/league/jogobonito/service/PlayerService.java
@@ -4,6 +4,8 @@ import com.league.jogobonito.dto.PlayerDTO;
 
 public interface PlayerService {
     PlayerDTO guardarNuevoPlayer(PlayerDTO playerDTO) throws Exception;
+    //AQUI AGREGAR EL LIST Y ESAS COSAS
     PlayerDTO buscarPlayerPorId(Integer id) throws Exception;
     PlayerDTO modificarPlayer(PlayerDTO playerDTO) throws Exception;
+
 }
